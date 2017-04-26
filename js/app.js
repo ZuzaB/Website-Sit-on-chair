9e79d87f22288352c8d8029fadd8891e3df53df5
ZuzaB <lacznaz@wp.pl>
2017-04-26 22:46:20 +0200

    add slider to top section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+    //menue rozwijane
     var topMenu = document.querySelector('ul');
     var li = topMenu.querySelectorAll('nav>ul>li');
-    
     
     for(var i=0; i<li.length; i++) {
         li[i].addEventListener('mouseover', function(){
@@ -21,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         });
     }
-    
+    //znikające opisy
     var products = document.querySelectorAll('.products');
    
    for(var i=0; i<products.length; i++) {
@@ -43,4 +42,36 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         });     
    }
+    //slider
+    var buttonNext = document.querySelector(".right");
+    var buttonPrev = document.querySelector(".left");
+    var slider = document.querySelector('.slider');
+    var lis = slider.querySelectorAll("li");
+    var index = 0;
+    
+    console.log(buttonNext, buttonPrev,slider, lis, index);
+    
+    lis[index].classList.remove("hidden");
+    
+    buttonNext.addEventListener('click', function () {
+
+        lis[index].classList.add("hidden");
+        index++;
+        if (index === lis.length) {
+            index = 0;
+        }
+        
+        lis[index].classList.remove("hidden");
+    });
+    
+     buttonPrev.addEventListener('click', function () {
+                 
+        lis[index].classList.add("hidden");
+        index--;
+        if (index < 0) {
+            index = lis.length - 1;
+        }
+
+        lis[index].classList.remove("hidden");
+    });
 });
